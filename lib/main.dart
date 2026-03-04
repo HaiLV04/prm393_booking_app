c5c1ac1bf81d42faaacc7379fd152ae5af9067b9
Phuong Nam <namphhe181352@fpt.edu.vn>
2026-03-04 20:55:00 +0700

    push màn đăng nhập và đăng ký

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:prm393_booking_app/features/customer_home/presentation/screens/customer_home_screen.dart';
-import 'package:prm393_booking_app/theme/app_theme.dart';
+import 'package:prm393_booking_app/screen/login_screen.dart';
 
 void main() {
   runApp(const GourmetHavenApp());
@@ -9,13 +8,35 @@ void main() {
 class GourmetHavenApp extends StatelessWidget {
   const GourmetHavenApp({super.key});
 
+  static const Color _primary = Color(0xFF13EC5B);
+  static const Color _bgLight = Color(0xFFF6F8F6);
+  static const Color _bgDark = Color(0xFF102216);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Restaurant Table Management',
       debugShowCheckedModeBanner: false,
-      title: 'Gourmet Haven',
-      theme: AppTheme.light,
-      home: const CustomerHomeScreen(),
+      theme: ThemeData(
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: _bgLight,
+        colorScheme: const ColorScheme.light(
+          primary: _primary,
+          surface: _bgLight,
+        ),
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: _bgDark,
+        colorScheme: const ColorScheme.dark(
+          primary: _primary,
+          surface: _bgDark,
+        ),
+        useMaterial3: true,
+      ),
+      themeMode: ThemeMode.dark,
+      home: const LoginScreen(),
     );
   }
 }
